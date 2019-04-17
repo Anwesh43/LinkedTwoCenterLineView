db35db65abd1952d22f0d17adf424cbf63f9a478
Anwesh43 <anweshthecool0@gmail.com>
2019-04-17 10:39:38 +0530

    adding TCLNode class

--- twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
+++ twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
@@ -117,4 +117,48 @@ class TwoCenterLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TCLNode(var i : Int, val state : State = State()) {
+
+        private var next : TCLNode? = null
+        private var prev : TCLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TCLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTCLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float)  -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TCLNode {
+            var curr : TCLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
