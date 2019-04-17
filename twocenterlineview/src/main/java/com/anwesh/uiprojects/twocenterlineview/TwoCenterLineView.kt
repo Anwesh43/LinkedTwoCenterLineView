6d39cf9cb8139bd4e1a988c78d56057c5482b1a3
Anwesh43 <anweshthecool0@gmail.com>
2019-04-17 10:32:28 +0530

    adding TwoCCenterLineView

--- twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
+++ twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
@@ -57,3 +57,21 @@ fun Canvas.drawTCLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TwoCenterLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
