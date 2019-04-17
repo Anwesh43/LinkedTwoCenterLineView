235cee6e3cfc9d3e6ad94ec6f17e7c1bb2e7870e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-17 10:41:48 +0530

    adding TwoCenterLine class

--- twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
+++ twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
@@ -161,4 +161,28 @@ class TwoCenterLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TwoCenterLine(var i : Int) {
+
+        private val root : TCLNode = TCLNode(0)
+        private var curr : TCLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
