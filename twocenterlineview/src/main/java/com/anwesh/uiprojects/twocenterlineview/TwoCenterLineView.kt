a8ac56f772afda4e295c4cd7eafb70ce0120ea8b
Anwesh43 <anweshthecool0@gmail.com>
2019-04-17 09:51:16 +0530

    adding libraries and constants

--- twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
+++ twocenterlineview/src/main/java/com/anwesh/uiprojects/twocenterlineview/TwoCenterLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.twocenterlineview
 /**
  * Created by anweshmishra on 17/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val second_part_size : Int = 2
+val first_part_size : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
