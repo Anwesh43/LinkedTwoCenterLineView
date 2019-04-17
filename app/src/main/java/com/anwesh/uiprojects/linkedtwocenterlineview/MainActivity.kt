da9aa187656261ad6b1e290258baba55e1466a82
Anwesh43 <anweshthecool0@gmail.com>
2019-04-17 10:46:02 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtwocenterlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtwocenterlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtwocenterlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.twocenterlineview.TwoCenterLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TwoCenterLineView.create(this)
     }
 }
